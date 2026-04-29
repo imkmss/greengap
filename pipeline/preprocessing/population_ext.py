9dba088a85480bf16e6a275f48ab690bc6e359d7
kangmyeongseo <signkms14@gmail.com>
2026-04-29 12:03:44 +0900

    한글 인코딩 수정

--- pipeline/preprocessing/population_ext.py
+++ pipeline/preprocessing/population_ext.py
@@ -8,7 +8,7 @@ result = df[['행정동코드', '생활인구합계']]
 # 컬럼 이름 정리
 result.rename(columns={'생활인구합계': 'population'}, inplace=True)
 
-result.to_csv("../../output/population_clean.csv", index=False, encoding="cp949")
+result.to_csv("../../output/population_clean.csv", index=False, encoding="utf-8-sig")
 
-output = pd.read_csv("../../output/population_clean.csv", encoding="cp949")
+output = pd.read_csv("../../output/population_clean.csv", encoding="utf-8-sig")
 print(output.head(20).to_string())
